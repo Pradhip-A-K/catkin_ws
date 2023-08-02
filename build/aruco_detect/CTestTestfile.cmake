276d389b9a20e2aef262b4394abb2048702a717a
Pradhip-A-K <113919151+Pradhip-A-K@users.noreply.github.com>
2023-08-03 00:23:55 +0530

    Add files via upload

---
+++ build/aruco_detect/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pradhip/catkin_ws/src/aruco_detect
+# Build directory: /home/pradhip/catkin_ws/build/aruco_detect
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
